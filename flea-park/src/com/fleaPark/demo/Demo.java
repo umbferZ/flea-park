316617a6e0245c731d32723b41b0dccbbe6e8550
umbferZ <umberto.ferracci@gmail.com>
2017-01-11 23:20:37 +0100

    new new

--- flea-park/src/com/fleaPark/demo/Demo.java
+++ flea-park/src/com/fleaPark/demo/Demo.java
@@ -3,5 +3,6 @@ package com.fleaPark.demo;
 public class Demo {
 	public static void main(String args[]) {
 		System.out.println("Hello world!\nI'am Flea Park!");
+		System.out.println("This is a new commit");
 	}
 }
